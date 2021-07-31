757deb36c032a855253a6322fec80152b3f065dc
peterkabz <50104012+peterkabz@users.noreply.github.com>
2021-07-31 22:51:43 +0200

    initial script

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from common import file_names
 from common.io import csv_reader
 from services import waste_2018_2019_processor
+from services import waste_stats_processor
 
 
-waste_2018_2019_processor.get_energy_saved()
+results = waste_2018_2019_processor.get_energy_saved()
+results2 = waste_stats_processor.get_energy_saved()
+
+# TODO combine the results
 
 
